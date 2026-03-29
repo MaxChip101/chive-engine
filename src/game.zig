b4135f7db73bad96642799f89e28281906cc8f51
MaxChip <1Max0Lag1@gmail.com>
2026-03-29 15:54:06 -0400

    working on how the game is being structured

--- src/game.zig
+++ src/game.zig
@@ -31,12 +31,23 @@ pub const Game = struct {
         return try fs.path.joinZ(self.allocator, &[_][]const u8{ self.binary_directory, file });
     }
 
-    pub fn read_file_from_path(self: *Self, path: []const u8) ![:0]const u8 {
+    pub fn read_file_from_path(self: *Self, path: []const u8) ![]u8 {
         var file = try fs.openFileAbsolute(path, .{});
         defer file.close();
 
         const file_size = try file.getEndPos();
         const content = try file.readToEndAlloc(self.allocator, file_size);
         return content;
+    }
+
+    pub fn read_file_from_pathZ(self: *Self, path: []const u8) ![:0]const u8 {
+        var file = try fs.openFileAbsolute(path, .{});
+        defer file.close();
+
+        const file_size = try file.getEndPos();
+        const content = try file.readToEndAlloc(self.allocator, file_size);
+        defer self.allocator.free(content);
+        const contentZ = try self.allocator.dupeZ(u8, content);
+        return contentZ;
     }
 };
--- src/objects.zig
+++ src/objects.zig
@@ -1,16 +1,12 @@
 const vectors = @import("vectors.zig");
 const rl = @import("raylib");
 
-pub const ComponentType = enum {
-    sprite_object,
-    audio_source,
-};
-
-pub const Component = union(ComponentType) {
+pub const Component = union(enum) {
     sprite_object: struct {
         transform: vectors.Transform,
         sprite: rl.Texture2D,
     },
+    camera_object: struct { position: vectors.Vec2, zoom: f32, camera: rl.Camera2D },
     audio_source: struct {
         position: vectors.Vec2,
         audio: rl.Sound,
