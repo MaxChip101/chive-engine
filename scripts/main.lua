2aef1ce8779a1ad669ca959a0351180ee19ad3a9
MaxChip101 <1max0lag1@gmail.com>
2026-05-31 13:49:23 -0400

    added stuff for lsp

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,3 +1,42 @@
+---@class Vec2
+---@field x number
+---@field y number
+
+---@class Vec3
+---@field x number
+---@field y number
+---@field z number
+
+---@class Color
+---@field r number
+---@field g number
+---@field b number
+---@field a number
+
+---@class DisplayMethodEnum
+---@field Windowed 0
+---@field FullScreen 1
+---@field Borderless 2
+---@field BorderlessWindowed 3
+
+---@class TextureTypeEnum
+---@field Stretch 0
+---@field Tile 1
+
+---@class chive
+---@field Setup fun(title: string, fps: integer, width: integer, height: integer, display_method: integer, resolution_width: integer, resolution_height: integer, texture_atlas_size: integer, texture_atlas_count: integer)
+---@field CreateScene fun(): integer
+---@field SetScene fun(scene_id: integer)
+---@field LoadTextureAtlas fun(filename: string): integer
+---@field CreateTexture fun(atlas_id: integer, uv_min: Vec2, uv_max: Vec2, texture_type: integer, flip_u: boolean, flip_v: boolean, tint: Color, size: Vec2): integer
+---@field CreateCamera fun(position: Vec3, rotation: Vec3, fov: number): integer
+---@field SetCamera fun(camera_id: integer)
+---@field CreateSurface fun(scene_id: integer, position: Vec3, normal: Vec3, rotation: number, size: Vec2, texture_id: integer): integer
+
+---@type chive
+chive = chive
+
+---@type DisplayMethodEnum
 local DisplayMethod = {
     Windowed = 0,
     FullScreen = 1,
@@ -5,6 +44,7 @@ local DisplayMethod = {
     BorderlessWindowed = 3
 }
 
+---@type TextureTypeEnum
 local TextureType = {
     Stretch = 0,
     Tile = 1,
@@ -20,9 +60,8 @@ local display_method = DisplayMethod.Windowed
 local texture_atlas_size = 1024
 local texture_atlas_count = 1
 
--- calls at start
+--- Called once at startup. Must call chive.Setup first.
 function Start()
-    -- setup must be called at the start
     chive.Setup(title, fps, width, height, display_method, resolution_width, resolution_height, texture_atlas_size, texture_atlas_count)
     local scene = chive.CreateScene()
     chive.SetScene(scene)
@@ -33,12 +72,14 @@ function Start()
     local surface = chive.CreateSurface(scene, {x=0, y=0, z=2}, {x=0, y=0, z=-1}, 0, {x=2, y=2}, texture)
 end
 
--- calls every frame
+--- Called every frame.
+---@param delta number milliseconds since last frame
 function Update(delta)
     
 end
 
--- calls when engine updates
+--- Called every engine tick.
+---@param delta number milliseconds since last frame
 function Tick(delta)
     
 end
